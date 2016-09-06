c101683f18b577426b7e2bf81d4736ca9a180753
juan <jbgraug@gmail.com>
2016-09-06 21:17:50 +0200

    mfixed import for myGgraph"

--- app/main.ts
+++ app/main.ts
@@ -4,7 +4,7 @@
 // import { AppComponent } from './app.component';
 // import { TodoService } from './services';
 
-import {MyGraph} from './models/state/myGraph';
+import {MyGraph} from './myGraph';
 
 
 // bootstrap(AppComponent,[
